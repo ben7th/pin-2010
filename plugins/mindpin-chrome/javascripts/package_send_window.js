37ea029e43fd45029bf00f7aaffa62ce27ba118e
fushang318 <fushang318@gmail.com>
2010-11-18 14:30:25 +0800

    打包发送

--- plugins/mindpin-chrome/javascripts/package_send_window.js
+++ plugins/mindpin-chrome/javascripts/package_send_window.js
@@ -1,5 +1,43 @@
 var BG = chrome.extension.getBackgroundPage();
 
+// 把 data 转换成 xml 数据
+function bundle_data(data){
+  // 链接
+  var bundle_str = ""
+  $(data.links).each(function(){
+    bundle_str += '<link href="'+ this.href +'" text="' + this.text + '" />';
+  })
+  // rss
+  $(data.rsses).each(function(){
+    bundle_str += '<link href="'+ this.href +'" text="' + this.text + '" />';
+  })
+  // 图片
+  $(data.images).each(function(){
+    bundle_str += '<image src="' + this.src + '"/>';
+  })
+  return '<bundle>'+ bundle_str +'</bundle>'
+}
+
+// 把 data 显示出来
+function show_data(data){
+  var rsses_dom_str = ""
+  var links_dom_str = ""
+  var image_dom_str = ""
+  $(data.rsses).each(function(){
+    rsses_dom_str += "<a href='"+ this.href +"'>"+ this.text +"<a>"
+  });
+  $(data.links).each(function(){
+    links_dom_str += "<a href='"+ this.href +"'>"+ this.text +"<a>"
+  });
+  $(data.images).each(function(){
+    image_dom_str += "<img src='"+this.src+"' width='"+ this.width +"' height='"+this.height+"' />"
+  });
+  
+  $("#package_content").append(rsses_dom_str)
+  $("#package_content").append(links_dom_str)
+  $("#package_content").append(image_dom_str)
+}
+
 $(document).ready(function(){
 //   {
 //      rsses:[{href:"",text:""}],
@@ -8,10 +46,43 @@ $(document).ready(function(){
 //    }
   var data = BG.package_send_data;
   BG.package_send_data = null;
-
   // 获取工作空间
   Collection.get_workspaces_to_select();
-  
-  $("#package_content").html(data.rsses.length + data.links.length + data.images.length)
+  // 显示 data
+  show_data(data);
+
+  // 发送按钮事件
+  $("#send_btn").click(function(){
+    $("#tip").hide();
+    var comment = $("#send_content").attr("value") || "无标题"
+    var bundle_xml = bundle_data(data);
+    var content = bundle_xml + comment
+    var workspace_id = $("#workspaces").attr("value")
+
+
+    $("#tip").text("正在发送数据 ...")
+    $("#tip").show();
+
+    var success = function(url){
+      $(document.body).html("发送成功，<a href='"+ url +"' target='_blank'>查看详情<a>")
+    }
+    var auth = function(){
+      $("#tip").text("未知错误");
+      $("#tip").show();
+    }
+
+    $.ajax({
+      url: BG.Mindpin.SUBMIT_DISCUSSION_URL,
+      type: "post",
+      async: true,
+      data: {
+        workspace_id:workspace_id,
+        "text_pin[html]":content
+      },
+      dataType: "text",
+      success: success,
+      error: auth
+    });
+  });
 });
 
