5778d017e20ea79562c3aef327eb3eba79ed94f5
ben7th <ben7th@sina.com>
2010-11-25 01:08:33 +0800

    调整session配置

--- sites/pin-website/config/initializers/session_store.rb
+++ sites/pin-website/config/initializers/session_store.rb
@@ -1,12 +1,14 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying cookie session data integrity.
-# If you change this key, all old sessions will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-
-  ActionController::Base.session = {
-    :domain => "2010.mindpin.com", 
-    :key=>'_mindpin_2010_session_dev',
-    :secret=>'883abe7844502ee307e376fa4d0509253d7f9e55fc8be69a934735cd470cc8671af39e27482885960f3364fa8af420b5519571193e22987c3e9e4f9da29f15fb'
-  }
+case RAILS_ENV
+  when 'production'
+    ActionController::Base.session = {
+      :domain => "mindpin.com",
+      :key=>'_mindpin_session',
+      :secret=>'883abe7844502ee307e376fa4d0509253d7f9e55fc8be69a934735cd470cc8671af39e27482885960f3364fa8af420b5519571193e22987c3e9e4f9da29f15fb'
+    }
+  else
+    ActionController::Base.session = {
+      :domain => "mindpin.com", 
+      :key=>'_mindpin_session_devel',
+      :secret=>'883abe7844502ee307e376fa4d0509253d7f9e55fc8be69a934735cd470cc8671af39e27482885960f3364fa8af420b5519571193e22987c3e9e4f9da29f15fb'
+    }
+end
