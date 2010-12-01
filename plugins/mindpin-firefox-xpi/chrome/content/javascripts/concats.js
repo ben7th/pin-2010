f626e9aaaa52d81bdd81960dd61f9e43cdb632a7
chinacheng <chinachengliwen@gmail.com>
2010-12-01 17:52:12 +0800

    插件中删除联系人

--- plugins/mindpin-firefox-xpi/chrome/content/javascripts/concats.js
+++ plugins/mindpin-firefox-xpi/chrome/content/javascripts/concats.js
@@ -31,25 +31,50 @@ Mindpin.Concats = {
   // 组装用户
   create_concat_vbox : function(data){
     var concat_box = getSidebarWindow().document.createElement("vbox")
-    concat_box.setAttribute("align","start")
+    concat_box.setAttribute("align","start");
+    concat_box.setAttribute("class","concat_box");
     if(typeof(data[0].email)=="undefined"){
-      var label = getSidebarWindow().document.createElement("label")
-      label.setAttribute("value",data[1].email)
-      concat_box.appendChild(label)
+      var label = getSidebarWindow().document.createElement("label");
+      label.setAttribute("value",data[1].email);
+      concat_box.appendChild(label);
     }else{
-      var avatar = getSidebarWindow().document.createElement("image")
-      avatar.setAttribute("src",data[0].avatar)
-      avatar.setAttribute("width","32px")
-      avatar.setAttribute("height","32px")
-      var name_label = getSidebarWindow().document.createElement("label")
-      name_label.setAttribute("value",data[0].name)
-      var email_label = getSidebarWindow().document.createElement("label")
-      email_label.setAttribute("value",data[0].email)
-      concat_box.appendChild(avatar)
-      concat_box.appendChild(name_label)
-      concat_box.appendChild(email_label)
+      Mindpin.Concats.add_element_to_concatbox(concat_box,data);
     }
+    Mindpin.Concats.add_delete_button_to_concatbox(concat_box,data[1].id);
     return concat_box
+  },
+
+  add_element_to_concatbox : function(concat_box,data){
+    var avatar = getSidebarWindow().document.createElement("image");
+    avatar.setAttribute("src",data[0].avatar);
+    avatar.setAttribute("width","32px");
+    avatar.setAttribute("height","32px");
+    var name_label = getSidebarWindow().document.createElement("label");
+    name_label.setAttribute("value",data[0].name);
+    var email_label = getSidebarWindow().document.createElement("label");
+    email_label.setAttribute("value",data[0].email);
+    concat_box.appendChild(avatar);
+    concat_box.appendChild(name_label);
+    concat_box.appendChild(email_label);
+  },
+
+  add_delete_button_to_concatbox : function(concat_box,concat_id){
+    var delete_label = getSidebarWindow().document.createElement("label");
+    delete_label.setAttribute("value","删除");
+    delete_label.setAttribute("class","text-link");
+    delete_label.addEventListener('click',function(){
+      $.ajax({
+      url: Mindpin.DESTROY_CONCAT_URL+"?id="+concat_id,
+      type: "delete",
+      success: function(data){
+        $(delete_label.parentNode).remove();
+      },
+      error: function(xhr,text,error){
+        alert("删除失败")
+      }
+    });
+    },false);
+    concat_box.appendChild(delete_label);
   },
 
   // 添加联系人
@@ -61,18 +86,17 @@ Mindpin.Concats = {
       type: "post",
       data:{
         email:email.value
-        },
+      },
       success: function(data){
-        Mindpin.Log.m(data)
-        error_message.setAttribute("style","display:none")
+        error_message.setAttribute("style","display:none");
         email.value = "";
-        var concats = getSidebarWindow().document.getElementById("concats")
-        var concat_box = Mindpin.Concats.create_concat_vbox(data)
+        var concats = getSidebarWindow().document.getElementById("concats");
+        var concat_box = Mindpin.Concats.create_concat_vbox(data);
         concats.appendChild(concat_box);
       },
       error: function(xhr,text,error){
-        error_message.setAttribute("style","")
-        error_message.setAttribute("value",xhr.responseText)
+        error_message.setAttribute("style","");
+        error_message.setAttribute("value",xhr.responseText);
       }
     });
   }
