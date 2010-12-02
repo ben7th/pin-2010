ac9375c583fc6deeece6fe79528e53353fde2623
ben7th <ben7th@sina.com>
2010-12-03 00:23:56 +0800

    导图重定向

--- sites/pin-user-auth/app/controllers/misc_controller.rb
+++ sites/pin-user-auth/app/controllers/misc_controller.rb
@@ -7,6 +7,6 @@ class MiscController < ActionController::Base
 
   def old_map_redirect
     id = params[:id]
-    redirect_to pin_url_for 'pin-app-adapter',"/app/mindmap_editor/mindmaps/#{id}"
+    redirect_to pin_url_for 'pin-mindmap-editor',"/mindmaps/#{id}"
   end
 end
