8db67576f2ed9d269d9f856f587f014758eb79c9
ben7th <ben7th@sina.com>
2011-02-09 09:17:55 +0800

    2011春节

--- sites/pin-user-auth/db/migrate/20110110055615_rename_concats_to_contacts.rb
+++
@@ -1,9 +0,0 @@
-class RenameConcatsToContacts < ActiveRecord::Migration
-  def self.up
-    rename_table(:concats, :contacts)
-  end
-
-  def self.down
-    rename_table(:contacts,:concats)
-  end
-end
