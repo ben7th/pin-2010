d1432119345ddc2447896afd9545df1807f88160
ben7th <ben7th@sina.com>
2011-03-01 15:29:16 +0800

    2月综合改版

--- sites/pin-user-auth/lib/sina_weibo.rb
+++
@@ -1,7 +0,0 @@
-class SinaWeibo
-  SETTINGS = CoreService.find_setting_by_project_name(CoreService::USER_AUTH)
-  CALLBACK_URL = SETTINGS["sina_callback_url"]
-  API_KEY = SETTINGS["sina_api_key"]
-  API_SECRET = SETTINGS["sina_api_secret"]
-
-end
