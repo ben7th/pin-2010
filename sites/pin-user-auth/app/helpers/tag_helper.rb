5e9a72135555aa0e52c1c1d539c2298feba3f7a3
ben7th <ben7th@sina.com>
2011-05-20 15:02:11 +0800

    fixed:关键词名称空间

--- sites/pin-user-auth/app/helpers/tag_helper.rb
+++ sites/pin-user-auth/app/helpers/tag_helper.rb
@@ -2,13 +2,14 @@ module TagHelper
 
   def tag_label(tag)
     tag_name = tag.name
+    tag_full_name = tag.full_name
 
     tag_namespace_label = tag.namespace.blank? ? '':"<span class='namespace'>#{tag.namespace}</span>"
 
     if tag.has_logo?
-      link_to "#{tag_namespace_label}#{logo tag,:mini}#{tag_name}","/tags/#{tag_name}",:class=>'tag has-logo'
+      link_to "#{tag_namespace_label}#{logo tag,:mini}#{tag_name}","/tags/#{tag_full_name}",:class=>'tag has-logo'
     else
-      link_to "#{tag_namespace_label}#{tag_name}","/tags/#{tag_name}",:class=>'tag'
+      link_to "#{tag_namespace_label}#{tag_name}","/tags/#{tag_full_name}",:class=>'tag'
     end
 
   rescue Exception => ex
