5d07595c7174f54f1eb242ae0350a0bdccf5d3df
ben7th <ben7th@sina.com>
2011-05-22 19:43:14 +0800

    fixed:tag的显示

--- sites/pin-user-auth/app/helpers/tag_helper.rb
+++ sites/pin-user-auth/app/helpers/tag_helper.rb
@@ -1,15 +1,16 @@
 module TagHelper
 
   def tag_label(tag)
-    tag_name = tag.name
     tag_full_name = tag.full_name
 
     tag_namespace_label = tag.namespace.blank? ? '':"<span class='namespace'>#{tag.namespace}</span>"
+    tag_name_label = "<span class='tag-name'>#{tag.name}<span>"
 
     if tag.has_logo?
-      link_to "#{tag_namespace_label}#{logo tag,:mini}#{tag_name}","/tags/#{tag_full_name}",:class=>'tag has-logo'
+      tag_logo_label = logo(tag,:mini)
+      link_to "#{tag_namespace_label}#{tag_logo_label}#{tag_name_label}","/tags/#{tag_full_name}",:class=>'tag has-logo'
     else
-      link_to "#{tag_namespace_label}#{tag_name}","/tags/#{tag_full_name}",:class=>'tag'
+      link_to "#{tag_namespace_label}#{tag_name_label}","/tags/#{tag_full_name}",:class=>'tag'
     end
 
   rescue Exception => ex
