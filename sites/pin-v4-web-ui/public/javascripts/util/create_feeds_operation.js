e0c5676c12630b2cd08f6890c19250049a0aabf7
ben7th <ben7th@sina.com>
2011-07-30 14:38:49 +0800

    主题发送开启

--- sites/pin-v4-web-ui/public/javascripts/util/create_feeds_operation.js
+++ sites/pin-v4-web-ui/public/javascripts/util/create_feeds_operation.js
@@ -168,3 +168,15 @@ pie.load(function(){
 
   }
 })
+
+pie.load(function(){
+  jQuery(document).delegate('.page-new-feed-form .create-submit','click',function(){
+    var elm = jQuery(this);
+    var form_elm = elm.closest('form');
+    if(form_elm.find('.sendto-hid').val() == ''){
+      alert('请选择发送范围');
+      return;
+    }
+    form_elm.submit();
+  })
+})
