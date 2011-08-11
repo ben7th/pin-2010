de62291627c3ee4536173e3c0097763521d4989b
ben7th <ben7th@sina.com>
2011-08-11 17:29:02 +0800

    文件html5上传

--- sites/pin-user-auth/app/controllers/photos_controller.rb
+++ sites/pin-user-auth/app/controllers/photos_controller.rb
@@ -2,7 +2,7 @@ class PhotosController < ApplicationController
   def create
     photo = current_user.photos.create(:image=>params[:file])
     unless photo.id.blank?
-      return render :text=>"上传成功"
+      return render :partial=>'modules/photos/photo_manage',:locals=>{:photos=>[photo]}
     end
     render :text=>"上传失败",:status=>402
   end
