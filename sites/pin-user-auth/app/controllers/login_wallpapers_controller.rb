f94adebb58d2be3056a4cc5e8fb2b495270a1e05
ben7th <ben7th@sina.com>
2011-12-24 02:34:47 +0800

    wallpapers

--- sites/pin-user-auth/app/controllers/login_wallpapers_controller.rb
+++ sites/pin-user-auth/app/controllers/login_wallpapers_controller.rb
@@ -1,11 +1,11 @@
 class LoginWallpapersController < ApplicationController
-  before_filter :login_required
+  # before_filter :login_required
 
   def new
   end
 
   def create
-    LoginWallpaper.create!(:image=>params[:image],:title=>params[:title])
+    LoginWallpaper.create!(:image=>params[:image], :title=>params[:title])
     redirect_to :action=>:new
   end
 
@@ -20,7 +20,7 @@ class LoginWallpapersController < ApplicationController
   end
 
   def get_next_wallpaper
-    @login_wallpaper = LoginWallpaper.find(:first,:order=>"id asc",:conditions=>"id > #{params[:id]}")
+    @login_wallpaper = LoginWallpaper.find(:first,:order=>"id asc", :conditions=>['id > ?', params[:id]])
     if @login_wallpaper.blank?
       @login_wallpaper = LoginWallpaper.find(:first,:order=>"id asc")
     end
@@ -28,7 +28,7 @@ class LoginWallpapersController < ApplicationController
   end
 
   def get_prev_wallpaper
-    @login_wallpaper = LoginWallpaper.find(:first,:order=>"id desc",:conditions=>"id < #{params[:id]}")
+    @login_wallpaper = LoginWallpaper.find(:first,:order=>"id desc", :conditions=>['id < ?', params[:id]])
     if @login_wallpaper.blank?
       @login_wallpaper = LoginWallpaper.find(:first,:order=>"id desc")
     end
@@ -39,16 +39,16 @@ class LoginWallpapersController < ApplicationController
   def render_wallpaper_info
     cookies[:login_wallpaper_id] = @login_wallpaper.id
     render :json=>{
-         :id=>@login_wallpaper.id,
-         :width=>@login_wallpaper.image.width,
-         :height=>@login_wallpaper.image.height,
-         :title=>@login_wallpaper.title,
-         :src=>@login_wallpaper.image.url
+       :id     => @login_wallpaper.id,
+       :width  => @login_wallpaper.image.width,
+       :height => @login_wallpaper.image.height,
+       :title  => @login_wallpaper.title,
+       :src    => @login_wallpaper.image.url
     }
   end
 
 
-#  GET /login/get_next_wallpaper?id=xxx
-#GET /login/get_prev_wallpaper?id=xxx
+# GET /login/get_next_wallpaper?id=xxx
+# GET /login/get_prev_wallpaper?id=xxx
 #
 end
