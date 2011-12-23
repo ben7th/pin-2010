28f96880d90ee8fbc6c7704ce545e068505f977d
ben7th <ben7th@sina.com>
2011-12-23 23:13:33 +0800

    comment bugfix

--- sites/pin-v4-web-ui/public/javascripts/util/feed_show_comment.js
+++ sites/pin-v4-web-ui/public/javascripts/util/feed_show_comment.js
@@ -4,9 +4,8 @@ pie.load(function(){
   if(jQuery('.page-feed-show-comments').length == 0) return;
 
   var comments_elm = jQuery('.page-feed-show-comments');
-  
-  var form_elm = jQuery('.page-feed-show-comments .comment-form');
-  var ipter_elm = form_elm.find('textarea.comment-ipter');
+  var form_elm     = comments_elm.find('.comment-form');
+  var ipter_elm    = form_elm.find('textarea.comment-ipter');
   ipter_elm.val('');
 
   // 发评论
@@ -20,10 +19,8 @@ pie.load(function(){
    * 不符合条件的，都认为是普通的新增评论
    */
 
-  jQuery('.page-feed-show-comments .comment-form a.commit').bind('click',function(){
-    var elm = jQuery(this);
-    var form_elm = elm.closest('.comment-form');
-    var content = ipter_elm.val().strip();
+  form_elm.find('a.commit').click(function(){
+    var content  = jQuery.string(ipter_elm.val()).strip().str;
 
     //参数检查
     if(jQuery.string(content).blank()){
@@ -36,22 +33,21 @@ pie.load(function(){
     }
 
     // 判断是回复评论，还是新增评论
-    var reply_comment_id = ipter_elm.domdata('reply-comment-id');
-    var be_replied_elm = comments_elm.find('.comments .comment[data-id='+reply_comment_id+']');
+    var reply_comment_id = ipter_elm.data('reply-comment-id');
+    var be_replied_elm   = comments_elm.find('.comments .comment[data-id='+reply_comment_id+']');
+    
     if(be_replied_elm.length > 0){
-      var user_name = be_replied_elm.domdata('user-name');
-      var prefix = '回复@'+user_name+':';
-      var is_reply = jQuery.string(content).startsWith(prefix);
+      var user_name = be_replied_elm.data('user-name');
+      var prefix    = '回复@'+user_name+':';
+      var is_reply  = jQuery.string(content).startsWith(prefix);
       if(is_reply){
-        pie.log('reply');
-
         // 回复评论
         jQuery.ajax({
-          url : '/post_comments/reply',
-          type : 'post',
-          data : 'reply_comment_id='+reply_comment_id+'&content='+encodeURIComponent(content),
-          beforeSend : function(){
-
+          url  : '/post_comments/reply',
+          type : 'POST',
+          data : {
+            'reply_comment_id' : reply_comment_id,
+            'content' : content
           },
           success : function(res){
             comment_success(res);
@@ -62,16 +58,13 @@ pie.load(function(){
       }
     }
 
-
-
     // 新增评论
-    var feed_id = form_elm.domdata('feed-id');
+    var feed_id = form_elm.data('feed-id');
     jQuery.ajax({
-      url : '/feeds/'+feed_id+'/comments',
-      type : 'post',
-      data : 'content='+encodeURIComponent(content),
-      beforeSend : function(){
-
+      url  : '/feeds/'+feed_id+'/comments',
+      type : 'POST',
+      data : {
+        'content' : content
       },
       success : function(res){
         comment_success(res);
@@ -80,35 +73,34 @@ pie.load(function(){
 
   })
 
-  function comment_success(res){
-      var new_comment_elm = jQuery(res).find('.comment');
+  var comment_success = function(res){
+    var new_comment_elm = jQuery(res).find('.comment');
 
-      ipter_elm.val('');
-      comments_elm.find('.comments .comment.blank').remove();
-      comments_elm.find('.comments').prepend(new_comment_elm);
+    ipter_elm.val('');
+    comments_elm.find('.comments .comment.blank').remove();
+    comments_elm.find('.comments').prepend(new_comment_elm);
 
-      new_comment_elm.hide().fadeIn(200);
+    new_comment_elm.hide().fadeIn(200);
 
-      var next_elm = new_comment_elm.next('.comment');
-      if( new_comment_elm.domdata('user-name') == next_elm.domdata('user-name') ){
-        next_elm.addClass('same-user');
-      }
+    var next_elm = new_comment_elm.next('.comment');
+    if( new_comment_elm.data('user-name') == next_elm.data('user-name') ){
+      next_elm.addClass('same-user');
+    }
   }
 
   // 删除评论
-  jQuery('.page-feed-show-comments .comment .ops a.delete').live('click',function(){
+  jQuery('.page-feed-show-comments .comment .ops a.delete').live('click', function(){
     var elm = jQuery(this);
-    elm.confirm_dialog('确定删除吗',function(){
+    elm.confirm_dialog('确定删除吗', function(){
       var comment_elm = elm.closest('.comment')
-
-      var comment_id = comment_elm.domdata('id');
+      var comment_id  = comment_elm.data('id');
       
       // DELETE
       jQuery.ajax({
         url : '/post_comments/'+comment_id,
-        type : 'delete',
+        type : 'DELETE',
         success : function(){
-          comment_elm.fadeOut(200,function(){
+          comment_elm.fadeOut(200, function(){
             comment_elm.next('.comment').removeClass('same-user');
             comment_elm.remove();
           })
@@ -118,14 +110,14 @@ pie.load(function(){
   })
 
   //回复评论
-  jQuery('.page-feed-show-comments .comment .ops a.reply').live('click',function(){
+  jQuery('.page-feed-show-comments .comment .ops a.reply').live('click', function(){
     var elm = jQuery(this);
     var comment_elm = elm.closest('.comment')
-    var user_name = comment_elm.domdata('user-name');
-    var reply_comment_id = comment_elm.domdata('id');
+    var user_name = comment_elm.data('user-name');
+    var reply_comment_id = comment_elm.data('id');
 
     var prefix = '回复@'+user_name+':';
-    ipter_elm.focus().val(prefix).domdata('reply-comment-id',reply_comment_id);
+    ipter_elm.focus().val(prefix).data('reply-comment-id', reply_comment_id);
   })
 })
 
